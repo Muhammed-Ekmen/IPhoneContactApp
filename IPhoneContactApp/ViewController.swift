544b5b3fbf62495e41d08fcdd4fd1feb1555f6fd
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-03-07 21:49:30 +0300

    Initial Commit

---
+++ IPhoneContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPhoneContactApp
+//
+//  Created by Semih Ekmen on 7.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
